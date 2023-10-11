24d63046907c8f715c10e58230eba6960f8b5215
Fady Fayek <f.fayek@siemat.sa>
2023-10-11 12:42:41 +0300

    otp setting

---
+++ RentZ.Domain/Entities/OtpSetup.cs
@@ -0,0 +1,15 @@
+﻿#nullable disable
+using System.ComponentModel.DataAnnotations;
+
+namespace RentZ.Domain.Entities;
+
+public class OtpSetup
+{
+    [Key]
+    public Guid Id { get; set; }
+    public string Code { get; set; }
+    public DateTime ExpiryDate { get; set; }
+
+    public virtual User User { get; set; }
+}
+
